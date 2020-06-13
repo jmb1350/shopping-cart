24d943c6d06414afce8187f1a85e4c673c2340d4
jmb1350 <65556581+jmb1350@users.noreply.github.com>
2020-06-13 09:04:00 -0400

    Stores selected products and totals after loop is complete

--- shopping_cart.py
+++ shopping_cart.py
@@ -56,6 +56,7 @@ def to_usd(my_price):
     return f("${my_price:,.2f}") #> $12,000.71
 
 total_price = 0
+selected_ids = []
 
 while True:
         selected_id = input("Please input a product code: ")
@@ -66,13 +67,20 @@ while True:
                 matching_product = matching_products[0]
                 total_price = total_price + matching_product["price"]
                 print("Selected Product: " + matching_product["name"] + " " + str(matching_product["price"]))
+                selected_ids.append(selected_id)
 
 
+#print(selected_ids)
+
+for selected_id in selected_ids:
+        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+        matching_product = matching_products[0]
+        total_price = total_price + matching_product["price"]
+        print("Selected Product: " + matching_product["name"] + " " + str(matching_product["price"]))
 
 #Display total price of products chosen
-
-
 print("Total Price: " + str(total_price))
 #make sure to format as USD
+
 
 
