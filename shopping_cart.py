d8db392fb52bb120cc3586bbfeb93181b1177c0e
jmb1350 <65556581+jmb1350@users.noreply.github.com>
2020-06-13 08:55:07 -0400

    Sums running total of prices chosen

--- shopping_cart.py
+++ shopping_cart.py
@@ -55,8 +55,7 @@ def to_usd(my_price):
     #"""
     return f("${my_price:,.2f}") #> $12,000.71
 
-# TODO: write some Python code here to produce the desired output
-
+total_price = 0
 
 while True:
         selected_id = input("Please input a product code: ")
@@ -65,5 +64,15 @@ while True:
         else:
                 matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
                 matching_product = matching_products[0]
+                total_price = total_price + matching_product["price"]
                 print("Selected Product: " + matching_product["name"] + " " + str(matching_product["price"]))
+
+
+
+#Display total price of products chosen
+
+
+print("Total Price: " + str(total_price))
+#make sure to format as USD
+
 
